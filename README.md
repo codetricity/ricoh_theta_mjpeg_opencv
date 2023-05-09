039d9c1a131b8fc07d4d5bf5339089b98d43df20
Craig <craig@oppkey.com>
2023-05-08 17:19:42 -0700

    working demo

---
+++ README.md
@@ -0,0 +1,40 @@
+# RICOH THETA OpenCV MJPEG Stream over WiFi
+
+author : [Robert Katter](https://community.theta360.guide/u/katterr)
+
+This is a copy of Robert Katter's script from the theta360.guide community
+forum. [Original post](https://community.theta360.guide/t/preview-mjpeg-stream-on-a-ricoh-theta-x-with-python-and-opencv/8919?u=craig)
+
+The image below is from Robert Katter.
+
+![robert katter screenshot](readme_assets/RicohMJPGPhyton.png)
+
+## Overview
+
+Use Python to view a motionjpeg stream in equirectangular format from RICOH
+THETA cameras.  This example uses OpenCV.
+
+## Usage
+
+1. connect camera in client mode
+1. change information below to the IP address and serial number of your camera
+1. if using pipenv, run:
+
+```text
+pipenv install
+pipenv shell
+python main.py
+```
+
+Refer to code and change lines below
+
+```python
+url = http://[Camera IP Address]/osc/commands/execute
+username = "CameraSerialNumber"
+password = "DigitsOnlyofCameraSerialNumber"
+
+# example information
+# url = 'http://192.168.2.101/osc/commands/execute'
+# username = "THETAYL00105377"
+# password = "00105377"
+```
---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from requests.auth import HTTPDigestAuth
+import cv2
+import numpy as np
+
+url = "http://[Camera IP Address]/osc/commands/execute"
+username = "CameraSerialNumber"
+password = "DigitsOnlyofCameraSerialNumber"
+
+# example information
+# url = 'http://192.168.2.101/osc/commands/execute'
+# username = "THETAYL00105377"
+# password = "00105377"
+
+payload = {
+    "name": "camera.getLivePreview"
+}
+
+headers = {
+    "Content-Type": "application/json;charset=utf-8"
+}
+
+response = requests.post(url, auth=HTTPDigestAuth(username, password), json=payload, headers=headers, stream=True)
+
+if response.status_code == 200:
+    bytes_ = bytes()
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            bytes_ += chunk
+            a = bytes_.find(b'\xff\xd8')
+            b = bytes_.find(b'\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = bytes_[a:b+2]
+                bytes_ = bytes_[b+2:]
+                img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                cv2.imshow("Preview", img)
+                if cv2.waitKey(1) == 27:
+                    break
+else:
+    print("Error: ", response.status_code)
+
+cv2.destroyAllWindows()
